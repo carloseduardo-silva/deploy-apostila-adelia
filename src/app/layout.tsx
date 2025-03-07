c95d7af37e8500f1878eba6963f15575076bf516
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2025-03-07 10:54:52 -0300

    fix: fonte nunito tag head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,10 @@
 import "./globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "./context/ThemeContext";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Apostila Editora Capro",
-  description: "Material Didático da Editora Capro",
-  icons:{
-    icon:"/assets/book-icon.png"
-  }
-  
-};
 
 export default function RootLayout({
   children,
@@ -22,11 +14,17 @@ export default function RootLayout({
   return (
     <ThemeProvider>
       <html lang="pt-BR">
-        <body className={inter.className}>{children}
+
+        <Head>
+          <title>Apostila Editora Capro</title>
+          <meta name="description" content="Material Didático da Editora Capro"></meta>
+          <link rel="icon" href="/assets/book-icon.png"></link>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet"></link>
-        </body>
+        </Head>
+
+        <body className={inter.className}>{children}</body>
         
       </html>
     </ThemeProvider>
